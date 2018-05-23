433c1322f12471dfaa2c6dd651f16880bb7b8705
shuangliz <358355029@qq.com>
2018-05-23 21:50:29 +0800

    2018.5.23

--- LightningPredict/src/router/index.js
+++ LightningPredict/src/router/index.js
@@ -1,18 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard'
 import Login from '@/components/Login'
+import DetectPage from '@/components/DetectPage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/Dashboard',
       name: 'Dashboard',
@@ -22,6 +17,15 @@ export default new Router({
       path:'/Login',
       name:'Login',
       component: Login
+    },
+    {
+      path:'/DetectPage',
+      name:'DetectPage',
+      component:DetectPage
+    },
+    {
+      path:'/',
+      redirect:'/DetectPage'
     }
   ]
 })
