43e5294e3f646d498f7c56882c98377662e6ceec
FoxerLee <foxerlee1@gmail.com>
2018-05-25 21:23:51 +0800

    add ter2 table

--- CassandraBackground/src/main/java/sse/tongji/thunder/CassandraConnector.java
+++ CassandraBackground/src/main/java/sse/tongji/thunder/CassandraConnector.java
@@ -4,6 +4,7 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import sse.tongji.thunder.model.Terminal;
+import sse.tongji.thunder.model.Terminal2;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -74,22 +75,52 @@ public class CassandraConnector {
         session.execute(query);
     }
 
+    public void createTerminal2Table(){
+        String query = "CREATE TABLE IF NOT EXISTS terminal2" +
+                "(" + "terminal_description text PRIMARY KEY, "+
+                "resistance float, "+
+                "earthing text,"+
+                "SPD text,"+
+                "kk text," +
+                "UA float," +
+                "UB float," +
+                "UC float," +
+                "IA float," +
+                "IB float," +
+                "IC float," +
+                "IrA float," +
+                "IrB float," +
+                "IrC float," +
+                "humidity float," +
+                "temperature float);";
+        session.execute(query);
+    }
+
     public void dropTable(String tableName){
         session.execute("DROP TABLE " + tableName + ";");
     }
 
-    public void insertIntoTerminal(){
+    public void insertTestData(){
+//        List<String> data = new ArrayList<String>();
+//        data.add("INSERT INTO terminal (terminal_description, peak, lasting_time, polarity, date, unit1, unit2)"
+//                + " VALUES('4#雷电分析仪',2.1,372,'正','2018-03-5 06:51:58','千安','微秒');");
+//        data.add("INSERT INTO terminal (terminal_description, peak, lasting_time, polarity, date, unit1, unit2)"
+//                + " VALUES('4#雷电分析仪',1.3,598,'正','2018-03-05 06:39:35','千安','微秒');");
+//        for(int i = 0; i < data.size(); i++){
+//            session.execute(data.get(i));
+//        }
+
         List<String> data = new ArrayList<String>();
-        data.add("INSERT INTO terminal (terminal_description, peak, lasting_time, polarity, date, unit1, unit2)"
-                + " VALUES('4#雷电分析仪',2.1,372,'正','2018-03-5 06:51:58','千安','微秒');");
-        data.add("INSERT INTO terminal (terminal_description, peak, lasting_time, polarity, date, unit1, unit2)"
-                + " VALUES('4#雷电分析仪',1.3,598,'正','2018-03-05 06:39:35','千安','微秒');");
-        for(int i = 0; i < data.size(); i++){
+        data.add("INSERT INTO terminal2 (terminal_description, resistance, earthing, SPD, kk, UA, UB, UC, IA, IB, IC,"
+                +"IrA, IrB, IrC, humidity, temperature)"
+                +" VALUES('4#雷电分析仪', 2.1, '是', '正常', '闭合', 227.9, 220.4, 230.1, 49.3, 52.2, 33.2,"
+                +"246.0, 60.0, 143.0, 7.1, 23.4)");
+        for (int i = 0; i < data.size(); i++){
             session.execute(data.get(i));
         }
     }
 
-    public List<Terminal> getAllData(){
+    public List<Terminal> getTerminalData(){
         ResultSet resultSet = session.execute("select terminal_description,peak,lasting_time,polarity,date,unit1,unit2 from terminal");
         List<Row> data = resultSet.all();
         System.out.println(data);
@@ -108,14 +139,45 @@ public class CassandraConnector {
         return dataset;
     }
 
+    public List<Terminal2> getTerminal2Data(){
+        ResultSet resultSet = session.execute("select terminal_description,resistance,earthing,SPD,kk,UA,UB,UC,IA,IB,IC,IrA,IrB,IrC,humidity,temperature from terminal2");
+        List<Row> data = resultSet.all();
+        System.out.println(data);
+        List<Terminal2> dataset = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++){
+            Terminal2 terminal = new Terminal2();
+            terminal.setTerminal_description(data.get(i).get(0, String.class));
+            terminal.setResistance(data.get(i).get(1, Float.class));
+            terminal.setEarthing(data.get(i).get(2, String.class));
+            terminal.setSPD(data.get(i).get(3, String.class));
+            terminal.setKk(data.get(i).get(4, String.class));
+            terminal.setUA(data.get(i).get(5, Float.class));
+            terminal.setUB(data.get(i).get(6, Float.class));
+            terminal.setUC(data.get(i).get(7, Float.class));
+            terminal.setIA(data.get(i).get(8, Float.class));
+            terminal.setIB(data.get(i).get(9, Float.class));
+            terminal.setIC(data.get(i).get(10, Float.class));
+            terminal.setIrA(data.get(i).get(11, Float.class));
+            terminal.setIrB(data.get(i).get(12, Float.class));
+            terminal.setIrC(data.get(i).get(13, Float.class));
+            terminal.setHumidity(data.get(i).get(14, Float.class));
+            terminal.setTemperature(data.get(i).get(15, Float.class));
+            dataset.add(terminal);
+        }
+
+        return dataset;
+    }
+
     public static void main(String[] args){
         CassandraConnector connector = new CassandraConnector();
         connector.connect("10.60.38.173", 9042);
         connector.createKeyspace("ThunderAnalysis", "SimpleStrategy", 1);
 //        connector.dropTable("terminal");
         connector.createTerminalTable();
-        connector.insertIntoTerminal();
-        connector.getAllData();
+        connector.createTerminal2Table();
+//        connector.insertTestData();
+        connector.getTerminal2Data();
+
         connector.close();
     }
 }
---
+++ CassandraBackground/src/main/java/sse/tongji/thunder/model/Terminal2.java
@@ -0,0 +1,155 @@
+package sse.tongji.thunder.model;
+
+
+
+public class Terminal2 {
+    private String terminal_description;
+    private Float resistance;
+    private String earthing;
+    private String SPD;
+    private String kk;
+    private Float UA;
+    private Float UB;
+    private Float UC;
+    private Float IA;
+    private Float IB;
+    private Float IC;
+    private Float IrA;
+    private Float IrB;
+    private Float IrC;
+    private Float humidity;
+
+    public void setTerminal_description(String terminal_description) {
+        this.terminal_description = terminal_description;
+    }
+
+    public void setResistance(Float resistance) {
+        this.resistance = resistance;
+    }
+
+    public void setEarthing(String earthing) {
+        this.earthing = earthing;
+    }
+
+    public void setSPD(String SPD) {
+        this.SPD = SPD;
+    }
+
+    public void setKk(String kk) {
+        this.kk = kk;
+    }
+
+    public void setUA(Float UA) {
+        this.UA = UA;
+    }
+
+    public void setUB(Float UB) {
+        this.UB = UB;
+    }
+
+    public void setUC(Float UC) {
+        this.UC = UC;
+    }
+
+    public void setIA(Float IA) {
+        this.IA = IA;
+    }
+
+    public void setIB(Float IB) {
+        this.IB = IB;
+    }
+
+    public void setIC(Float IC) {
+        this.IC = IC;
+    }
+
+    public void setIrA(Float irA) {
+        IrA = irA;
+    }
+
+    public void setIrB(Float irB) {
+        IrB = irB;
+    }
+
+    public void setIrC(Float irC) {
+        IrC = irC;
+    }
+
+    public void setHumidity(Float humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setTemperature(Float temperature) {
+        this.temperature = temperature;
+    }
+
+    private Float temperature;
+
+    public String getTerminal_description() {
+        return terminal_description;
+    }
+
+    public Float getResistance() {
+        return resistance;
+    }
+
+    public String getEarthing() {
+        return earthing;
+    }
+
+    public String getSPD() {
+        return SPD;
+    }
+
+    public String getKk() {
+        return kk;
+    }
+
+    public Float getUA() {
+        return UA;
+    }
+
+    public Float getUB() {
+        return UB;
+    }
+
+    public Float getUC() {
+        return UC;
+    }
+
+    public Float getIA() {
+        return IA;
+    }
+
+    public Float getIB() {
+        return IB;
+    }
+
+    public Float getIC() {
+        return IC;
+    }
+
+    public Float getIrA() {
+        return IrA;
+    }
+
+    public Float getIrB() {
+        return IrB;
+    }
+
+    public Float getIrC() {
+        return IrC;
+    }
+
+    public Float getHumidity() {
+        return humidity;
+    }
+
+    public Float getTemperature() {
+        return temperature;
+    }
+
+
+
+
+}
